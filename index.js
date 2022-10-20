26048d09a54e032e618f2ded185081090c0b14c8
sadmen rashik khan <sadmenrashik06@gmail.com>
2022-10-20 19:07:34 +0600

    server connect

--- index.js
+++ index.js
@@ -5,14 +5,35 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 const category = require('./data/category.json')
+const news = require('./data/news.json');
 
 app.get('/', (req,res)=>{
-    res.send('News API Rrsunning')
+    res.send('News API Running')
 });
 app.get('/news-category', (req,res)=>{
     res.send(category)
 });
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+})
+
+app.get('/news', (req, res) =>{
+    res.send(news);
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+});
 app.listen(port,()=>{
     console.log('Express News Server Running on Port ',port);
 })
